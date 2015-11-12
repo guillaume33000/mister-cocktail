90c2d696c2d777817704e84cb82300a9017e0010
Guillaume Campo <guillaume.campo@hotmail.fr>
2015-11-12 17:39:14 +0100

    cocktails creation ok, doses et ingredients nok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -4,7 +4,7 @@ class CocktailsController < ApplicationController
   end
 
   def show
-    @cocktail = Cocktail.find
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
@@ -12,5 +12,18 @@ class CocktailsController < ApplicationController
   end
 
   def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :doses)
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,33 @@
 class DosesController < ApplicationController
+
+before_action :find_cocktail, only: [ :new, :create ]
+
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = @cocktail.doses.build(dose_params)
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
 
-  def delete
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
   end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
+
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
 end
